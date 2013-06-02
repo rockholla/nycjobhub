b4379f572479db7293ae1d3fdbf148f89cb5ff88
rockholla <patrickforce@gmail.com>
2013-06-01 22:40:01 -0400

    init rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nycjobhub::Application.config.secret_token = '953208262dd30d2cf8208fa0f12db0289e3dd7f75d01fbf1ae3f51ff8e88849a768c150a9065fb3168a7f012b5622c82068323ee460ee4710ae27aca25eee4e2'
