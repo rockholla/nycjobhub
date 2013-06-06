721b6e15252e8cef412dec321f12087e155962d3
rockholla <patrickforce@gmail.com>
2013-06-06 17:10:41 -0400

    add listing search test

--- test/unit/connectors/nycjobs_test.rb
+++ test/unit/connectors/nycjobs_test.rb
@@ -8,6 +8,9 @@ class NycjobsTest < ActiveSupport::TestCase
      result     = connector.sync()
      assert_equal true, result.is_a?(Array)
      assert result.count > 0
+
+     search = Listing.search 'run'
+     assert_equal true, search.is_a?(Tire::Results::Collection)
   end
 
 end
